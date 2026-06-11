83d1b62d661361f26752da91ae4c7e9374d1e577
Tanny89k <sosunacastano87@gmail.com>
2026-06-11 13:24:05 -0400

    Update formats.ts

--- data/mods/monotypeorigins/formats.ts
+++ data/mods/monotypeorigins/formats.ts
@@ -3,11 +3,11 @@ import { FormatData } from '../../../sim/dex-formats';
 export const Formats: FormatData[] = [
 	{
 		name: "[Gen 9] Monotype: Origins", // Currently only the free form format is implemented
-		mod: 'monotypeorigins',
 		desc: `A micrometa featuring a single monotype mon for each type.`,
 		threads: [
 			`&bullet; <a href="https://www.smogon.com/forums/threads/solomods-megathread.3711007/page-8#post-10202864">Monotype: Origins on Smogon Forums</a>`,
 		],
+		mod: 'monotypeorigins',
 		ruleset: ['Standard'],
 		onValidateTeam(team, format) {
 			let speciesTable = {};
@@ -15,7 +15,7 @@ export const Formats: FormatData[] = [
 			for (const set of team) {
 				let template = this.dex.species.get(set.species);
 				if (!allowedTiers.includes(template.tier)) {
-					return [set.species + ' is not legal in Placeholder Mod.'];
+					return [set.species + ' is not legal in Monotype: Origins.'];
 				}
 			}
 		},
